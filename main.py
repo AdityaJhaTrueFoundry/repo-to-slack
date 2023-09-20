6a8ed5ac3b0d2a14e1f63d0d6450be1d3f24760e
Aditya Jha <adityajha@Adityas-MacBook-Air.local>
2023-09-20 09:48:52 +0530

    refactored code

--- main.py
+++ main.py
@@ -2,48 +2,89 @@ import os
 import requests
 from datetime import datetime, timedelta
 
-def get_activity_users(repo_owner, repo_name, activity_type):
+def get_activity_users(repo_owner, repo_name, activity_type, headers):
     if activity_type == "stargazers":
-        url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/stargazers"
+        stargazers_list = get_stargazers_with_pagination(repo_owner, repo_name, headers)
         timestamp_key = 'starred_at'
         action = 'star'
+        user_key = 'user'
     elif activity_type == "pull_requests":
         url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls"
         timestamp_key = 'created_at'
         action = 'opened a pull request'
+        user_key = 'owner'
     elif activity_type == "forks":
         url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/forks"
         timestamp_key = 'created_at'
+        user_key = 'owner'
         action = 'forked'
     else:
         print(f"Invalid activity type: {activity_type}")
         return []
 
-    headers = {
-        "Accept": "application/vnd.github.v3+json"
-    }
+    if activity_type != "stargazers":  # Only print the URL if it's not stargazers
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            activities = response.json()
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        activities = response.json()
-
-        # Get the current time
-        current_time = datetime.now()
-
-        # Define the time threshold (24 hours ago)
-        threshold_time = current_time - timedelta(hours=24)
-
-        # Filter users based on the activity timestamp
-        activity_users = [
-            activity['user']
-            for activity in activities
-            if datetime.strptime(activity[timestamp_key], "%Y-%m-%dT%H:%M:%SZ") > threshold_time
-        ]
-
-        return activity_users, action
+            # Filter users based on the activity timestamp
+            activity_users = filter_activity_users(user_key, timestamp_key, activities)
+            
+        else:
+            print(f"Failed to fetch {activity_type} for {repo_owner}/{repo_name}. Status code: {response.status_code}")
+            return [], ""
     else:
-        print(f"Failed to fetch {activity_type} for {repo_owner}/{repo_name}. Status code: {response.status_code}")
-        return [], ""
+        print("here")
+        # Use the stargazers_list that you fetched earlier
+        activity_users = filter_activity_users(user_key, timestamp_key, stargazers_list)
+    
+    return activity_users, action
+    
+def filter_activity_users(user_key, timestamp_key, activity_data):
+    # Get the current time
+    current_time = datetime.now()
+
+    # Define the time threshold (24 hours ago)
+    threshold_time = current_time - timedelta(hours=24)
+
+    return [
+        activity[user_key]
+        for activity in activity_data
+        if datetime.strptime(activity[timestamp_key], "%Y-%m-%dT%H:%M:%SZ") > threshold_time
+    ]
+
+def get_stargazers_with_pagination(repo_owner, repo_name, headers):
+    stargazers_list = []
+    next_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/stargazers?page=1"
+
+    while next_url:
+        response = requests.get(next_url, headers=headers)
+
+        if response.status_code == 200:
+            print(response.links)
+            stargazers = response.json()
+            if len(stargazers) == 0:
+                break  # No more stargazers to fetch
+
+            for stargazer in stargazers:
+                stargazers_dict = {
+                    "user": stargazer["user"],
+                    "starred_at": stargazer["starred_at"],
+                }
+                stargazers_list.append(stargazers_dict)
+
+            # Check if there is a 'next' URL in the response headers for pagination
+            if 'next' in response.links:
+                next_url = response.links['next']['url']
+                print(next_url)
+            else:
+                next_url = None  # No more pages to fetch
+
+        else:
+            print(f"Failed to fetch stargazers for {repo_owner}/{repo_name}. Status code: {response.status_code}")
+            break  # Stop fetching on error
+
+    return stargazers_list
 
 def format_user_profiles(users, action):
     formatted_profiles = []
@@ -71,12 +112,20 @@ def send_slack_message(webhook_url, message):
 # Read the repository list and activity types from environment variables
 repository_activity_list = os.getenv("REPOSITORY_ACTIVITY_LIST", "").split(",")
 
-# Provide your Slack webhook URL
+# Slack webhook URL
 slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL", "")
+
+# Get Github Authorization Token
+github_authorization_token = os.getenv("GITHUB_AUTH_TOKEN", "")
+
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {github_authorization_token}"
+}
 
 for repository_activity in repository_activity_list:
     repo_owner, repo_name, activity_type = repository_activity.strip().split("/")
-    activity_users, action = get_activity_users(repo_owner, repo_name, activity_type)
+    activity_users, action = get_activity_users(repo_owner, repo_name, activity_type, headers)
     if activity_users:
         formatted_profiles = format_user_profiles(activity_users, action)
         slack_message = f"Users who {action} {repo_owner}/{repo_name} in the Last 24 Hours:\n\n"
